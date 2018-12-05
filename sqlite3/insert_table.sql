5aeb002dc19a984a8fa57b73e08da380e2a86449
Husnain Bustam <Husnain.Bustam@priceline.com>
2018-12-05 12:25:40 -0500

    cleaned up files

---
+++ sqlite3/insert_table.sql
@@ -0,0 +1,3 @@
+-- CREATE TABLE friends (first_name TEXT, last_name TEXT, location TEXT, age INTEGER, birthday TEXT)
+
+CREATE TABLE population (first_name TEXT, last_name TEXT, location TEXT, age INTEGER, ssn TEXT)
